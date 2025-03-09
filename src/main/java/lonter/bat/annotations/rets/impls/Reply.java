6cf87fd6d103ce3ea74e0b1427e844426e72d968
samuele <samuele.fulcheri@finksolution.com>
2025-03-09 22:49:59 +0100

    Fix @Reply.

--- src/main/java/lonter/bat/annotations/rets/impls/Reply.java
+++ src/main/java/lonter/bat/annotations/rets/impls/Reply.java
@@ -2,19 +2,40 @@ package lonter.bat.annotations.rets.impls;
 
 import lonter.bat.annotations.rets.*;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 
+import java.awt.Color;
 import java.lang.annotation.Annotation;
 
 @ImplRet
 public final class Reply extends ReturnType {
+  @Value("${app.embedColor:#{null}}")
+  private String color;
+
   @Override
   public void action(final @NotNull MessageReceivedEvent e, final @NotNull Object output,
                      final @NotNull Annotation at) {
-    if(output instanceof String s && at instanceof lonter.bat.annotations.rets.ats.Reply reply) {
-      e.getMessage().reply(s).mentionRepliedUser(reply.value()).queue();
+    if(at instanceof lonter.bat.annotations.rets.ats.Reply reply) {
+      if(output instanceof String s)
+        e.getMessage().reply(s).mentionRepliedUser(reply.value()).queue();
+
+      else if(output instanceof EmbedBuilder embed) {
+        try {
+          embed.setColor(Color.decode(color));
+        } catch(final @NotNull Exception _) { }
+
+        e.getMessage().replyEmbeds(embed.build()).queue();
+      }
+
+      else {
+        System.err.println("The output type was not recognized.");
+        System.err.println(output);
+        System.err.println(at);
+      }
     }
   }
 }
